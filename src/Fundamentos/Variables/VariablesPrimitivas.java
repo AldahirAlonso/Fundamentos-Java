e57ab981289a3448b410ad347633217198090495
Aldahir Alonso <aldahir0603@hotmail.com>
2026-04-18 20:42:03 -0600

    Variables Primitivas.

---
+++ src/Fundamentos/Variables/VariablesPrimitivas.java
@@ -0,0 +1,23 @@
+package Fundamentos.Variables;
+
+public class VariablesPrimitivas {
+    public static void main(String[] args) {
+    byte age = 23;
+    short year = 2024;
+    long distanceToMoon = 384400000L;
+    float pi = 3.14F;
+    double price = 19.99F;
+    int xAxis, yAxis;
+    xAxis = 5;
+    yAxis = 5;
+    age = 30;
+        System.out.println("age: " + age);
+
+        System.out.println("xAxis = " + xAxis);
+        System.out.println("yAxis = " + yAxis);
+
+        char initial = '$';
+        System.out.println("initial = " + initial);
+        boolean isTeacher = true;
+    }
+}
