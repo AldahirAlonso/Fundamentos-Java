889a9ca4b23c8d9bb1566c2b1c4e5a1313ded2be
Aldahir Alonso <aldahir0603@hotmail.com>
2026-04-24 23:33:29 -0600

    Metodo estatico main.

---
+++ src/Clases_Objetos/PuntoEntrada/MainApp.java
@@ -0,0 +1,14 @@
+package Clases_Objetos.PuntoEntrada;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo.");
+
+
+        MainApp.sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.println("Hola soy la aplicacion principal.");
+    }
+}
