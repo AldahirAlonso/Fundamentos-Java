d80637a3571333439fbe45983ec55cde07c2940e
Aldahir Alonso <aldahir0603@hotmail.com>
2026-05-02 20:19:23 -0600

    Creando Sobrecarga de metodos start.

---
+++ src/POO/Vehicle/Vehicle.java
@@ -0,0 +1,68 @@
+package POO.Vehicle;
+
+public class Vehicle {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Vehicle(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void start() {
+        System.out.println("Encender el vehiculo...");
+    }
+
+    public void start(boolean silentMode) {
+        if (silentMode) {
+            System.out.println("Esta encendido en modo silencioso.");
+        } else {
+            System.out.println("Encendiento vehiculo");
+        }
+    }
+
+    public void stop() {
+        System.out.println("Apagando el vehiculo...");
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        int currentYear = java.time.Year.now().getValue();
+
+        if (year < 1886 || year > currentYear + 2) {
+            throw new IllegalArgumentException("Año Invalido!!!");
+        }
+
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
