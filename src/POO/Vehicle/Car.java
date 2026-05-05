36ca201817eb4b606763fd060e869e3afc1b7108
Aldahir Alonso <aldahir0603@hotmail.com>
2026-05-04 20:54:28 -0600

    Cambiar Herencia por Composicion en la lógica del motor.

--- src/POO/Vehicle/Car.java
+++ src/POO/Vehicle/Car.java
@@ -2,15 +2,17 @@ package POO.Vehicle;
 
 public class Car extends Vehicle {
     private int doors;
+    private Engine engine;
 
     public Car(String brand, String model, int year, int doors) {
         super(brand, model, year);
         this.doors = doors;
+        this.engine = new Engine();
     }
 
-    @Override
     public void start() {
         System.out.println("El auto esta encendido");
+        engine.start();
     }
 
     public int getDoors() {
