e9cf24b1e8bcb4f323fb5663bfc33273a22d8dec
Aldahir Alonso <aldahir0603@hotmail.com>
2026-05-04 22:11:29 -0600

    Creando interfaz con atributo constante y metodo abstracto.

---
+++ src/POO/ClasesAbstractasyInterfaces/Electric.java
@@ -0,0 +1,6 @@
+package POO.ClasesAbstractasyInterfaces;
+
+public interface Electric {
+    int MAX_BATTERY_CAPACITY = 100; // No es muy comun ver atributos en interfaces pero es posible.
+    void chargeBattery();
+}
