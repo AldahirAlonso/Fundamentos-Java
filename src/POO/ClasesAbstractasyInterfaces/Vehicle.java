d198ad289974aa0bce9348b05f5ec44eed73c880
Aldahir Alonso <aldahir0603@hotmail.com>
2026-05-04 21:48:44 -0600

    Creando clase abstracta.

---
+++ src/POO/ClasesAbstractasyInterfaces/Vehicle.java
@@ -0,0 +1,59 @@
+package POO.ClasesAbstractasyInterfaces;
+
+public abstract class Vehicle {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Vehicle(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void stop() {
+        System.out.println("Apagando el vehiculo...");
+    }
+
+    public abstract void drive();
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        int currentYear = java.time.Year.now().getValue();
+
+        if (year < 1886 || year > currentYear + 2) {
+            throw new IllegalArgumentException("Año Invalido!!!");
+        }
+
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
+
