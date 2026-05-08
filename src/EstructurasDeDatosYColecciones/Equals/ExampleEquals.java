660d70bed3b88cf8475b7cbeeab8bb5531144be7
Aldahir Alonso <aldahir0603@hotmail.com>
2026-05-07 20:11:23 -0600

    Ejemplo sencillo usando equals.

---
+++ src/EstructurasDeDatosYColecciones/Equals/ExampleEquals.java
@@ -0,0 +1,11 @@
+package EstructurasDeDatosYColecciones.Equals;
+
+public class ExampleEquals {
+    public static void main(String[] args) {
+        Integer numberA = 126;
+        Integer numberB = 126;
+
+        System.out.println(numberB == numberA);
+        System.out.println(numberB.equals(numberA));
+    }
+}
